a771df5f5613c54df7a6c6ec5807114e646b8e67
Selenophiles4879 <shreyanshtiwari12062003@gmail.com>
2024-06-09 17:19:15 +0530

    Add files via upload

---
+++ js.js
@@ -0,0 +1,7 @@
+const typed = new Typed('.multiple-text', {
+	strings: ['Web Designer' , 'Youtuber' , 'Video Editor' , 'Learner' ],
+	typeSpeed: 100,
+	backSpeed:100,
+	backDelay:1000,
+	loop:true,
+});
